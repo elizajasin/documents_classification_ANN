ac1243ae5c7aaac8e3d728417be5e10c0e440a3d
elizajasin <eliza_jasin@hotmail.com>
2017-05-24 13:55:36 +0700

    preprocessing 5

--- Main.py
+++ Main.py
@@ -1,13 +1,17 @@
 __author__ = 'elizajasin'
 
-import ReadData as ReadD
+import ReadAndWriteFile as RAWfile
 import Preprocessing as PreP
 
+# Read Data
+hadits = RAWfile.readData('hadits_fix.xlsx')
+
 # Preprocessing
-hadits = ReadD.readData('hadits_fix.xlsx')
 remove_punct = PreP.removePunct(hadits)
 case_fold = PreP.caseFolding(remove_punct)
 stopword_remove = PreP.stopwordsRemoval('id.stopwords.02.01.2016.txt',case_fold)
 lemma = PreP.lemmatization(stopword_remove)
+
+
 
 print(lemma)
