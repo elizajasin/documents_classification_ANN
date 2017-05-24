d2ec74611c64414d29d9e5d4c975d9ed282390c7
elizajasin <eliza_jasin@hotmail.com>
2017-05-24 18:00:32 +0700

    feature extraction 3

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -16,3 +16,16 @@ def sumFE (data, atribut):
             if data[i][j] in atribut.keys():
                 atribut[data[i][j]][i] += 1
     return atribut
+
+def normalisasi (atribut):
+    norm_atribut = {}
+    for key in atribut:
+        norm_atribut[key] = []
+    for key in atribut:
+        sum = 0
+        for i in range(len(atribut[key])):
+            sum += atribut[key][i]
+        for i in range(len(atribut[key])):
+            norm = atribut[key][i]/sum
+            norm_atribut[key].append(str(round(norm,2)))
+    return norm_atribut
