c9f877d97ccbd414a614b1d6232541c3485a26c3
elizajasin <eliza_jasin@hotmail.com>
2017-06-08 00:49:36 +0700

    add distribution data

---
+++ DataDistribution.py
@@ -0,0 +1,60 @@
+__author__ = 'elizajasin'
+
+import os
+import openpyxl
+from openpyxl import Workbook
+
+os.getcwd
+wb = openpyxl.load_workbook('hadits_fix.xlsx')
+ws = wb.active
+informasi = []
+anjuran = []
+larangan = []
+for i in range (2,ws.max_row+1):
+    if ws.cell(row=i,column=ws.max_column).value == 1:
+        informasi.append(str(ws.cell(row=i,column=ws.max_column-1).value))
+    elif ws.cell(row=i,column=ws.max_column).value == 2:
+        anjuran.append(str(ws.cell(row=i,column=ws.max_column-1).value))
+    else:
+        larangan.append(str(ws.cell(row=i,column=ws.max_column-1).value))
+
+wb = openpyxl.load_workbook('hadits_fix_2.xlsx')
+ws = wb.active
+for i in range (2,ws.max_row+1):
+    if ws.cell(row=i,column=ws.max_column).value == 1:
+        informasi.append(str(ws.cell(row=i,column=ws.max_column-1).value))
+    elif ws.cell(row=i,column=ws.max_column).value == 2:
+        anjuran.append(str(ws.cell(row=i,column=ws.max_column-1).value))
+    else:
+        larangan.append(str(ws.cell(row=i,column=ws.max_column-1).value))
+
+wb = openpyxl.load_workbook('hadits_fix_3.xlsx')
+ws = wb.active
+for i in range (2,ws.max_row+1):
+    if ws.cell(row=i,column=ws.max_column).value == 1:
+        informasi.append(str(ws.cell(row=i,column=ws.max_column-1).value))
+    elif ws.cell(row=i,column=ws.max_column).value == 2:
+        anjuran.append(str(ws.cell(row=i,column=ws.max_column-1).value))
+    else:
+        larangan.append(str(ws.cell(row=i,column=ws.max_column-1).value))
+
+wb = Workbook()
+ws = wb.active
+for i in range(172):
+    data = []
+    data.append(informasi[i])
+    data.append(1)
+    ws.append(data)
+    data = []
+    data.append(anjuran[i])
+    data.append(2)
+    ws.append(data)
+    data = []
+    data.append(larangan[i])
+    data.append(3)
+    ws.append(data)
+wb.save('hadits_dist_172.xlsx')
+
+print(len(informasi))
+print(len(anjuran))
+print(len(larangan))
--- MainFeatureExtraction.py
+++ MainFeatureExtraction.py
@@ -8,7 +8,8 @@ token = RAWfile.readDataPreprocessing("preprocessing_result.xlsx")
 
 # make feature extraction
 atribut = FE.makeAtribut(token)
-FE_result = FE.sumFE(token,atribut)
-RAWfile.writeFEResult(FE_result,"feature_extraction_result.xlsx")
-FE_norm = FE.normalisasi(FE_result)
-RAWfile.writeFEResult(FE_norm,"feature_extraction_norm.xlsx")
+print(atribut)
+# FE_result = FE.sumFE(token,atribut)
+# RAWfile.writeFEResult(FE_result,"feature_extraction_result.xlsx")
+# FE_norm = FE.normalisasi(FE_result)
+# RAWfile.writeFEResult(FE_norm,"feature_extraction_norm.xlsx")
