65cf3508056b4dccb8ec8be920d2088fb46a2bca
elizajasin <eliza_jasin@hotmail.com>
2017-06-10 08:18:32 +0700

    add bigram models

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -8,6 +8,19 @@ def makeAtribut (data):
                 atribut[data[i][j]] = []
     return atribut
 
+def makeAtributBigram (data):
+    atribut = {}
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if (j == 0) and (data[i][j]+'|<s>' not in atribut.keys()):
+                atribut[data[i][j]+'|<s>'] = []
+            else:
+                if data[i][j]+'|'+data[i][j-1] not in atribut.keys():
+                    atribut[data[i][j]+'|'+data[i][j-1]] = []
+                if (j == len(data[i])) and ('</s>|' + data[i][j] not in atribut.keys()):
+                    atribut['</s>|' + data[i][j]] = []
+    return atribut
+
 def sumFE (data, atribut):
     for i in range(len(data)):
         for key in atribut:
@@ -15,6 +28,20 @@ def sumFE (data, atribut):
         for j in range(len(data[i])):
             if data[i][j] in atribut.keys():
                 atribut[data[i][j]][i] += 1
+    return atribut
+
+def sumFEBigram (data, atribut):
+    for i in range(len(data)):
+        for key in atribut:
+            atribut[key].append(0)
+        for j in range(len(data[i])):
+            if (j == 0) and (data[i][j]+'|<s>' in atribut.keys()):
+                atribut[data[i][j]+'|<s>'][i] += 1
+            else:
+                if data[i][j]+'|'+data[i][j-1] in atribut.keys():
+                    atribut[data[i][j]+'|'+data[i][j-1]][i] += 1
+                if (j == len(data[i])) and ('</s>|' + data[i][j] in atribut.keys()):
+                    atribut['</s>|' + data[i][j]][i] += 1
     return atribut
 
 def normalisasi (atribut):
