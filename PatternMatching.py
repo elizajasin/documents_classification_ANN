19a896427ef41b9a5b743037f4e9f709f60d34e6
elizajasin <eliza_jasin@hotmail.com>
2017-06-15 16:35:11 +0700

    add pattern matching 2

--- PatternMatching.py
+++ PatternMatching.py
@@ -1,6 +1,7 @@
 __author__ = 'elizajasin'
 
 import ReadAndWriteFile as RAWfile
+from openpyxl import Workbook
 import re
 
 # Read Data
@@ -9,18 +10,22 @@ hadits = RAWfile.readData('hadits_dist_172.xlsx')
 # Matching Words
 classes = []
 for i in range(len(hadits)):
-    match1 = re.search(r'jangan', hadits[i])
-    match2 = re.search(r'larang', hadits[i])
-    match3 = re.search(r'lah', hadits[i])
+    match1 = re.search(r'jangan|larang', hadits[i])
+    match2 = re.search(r'hendaklah|lakukanlah|berdirilah|shalatlah|pergilah|berwudhulah', hadits[i])
 
     if match1:
         classes.append(3)
     elif match2:
-        classes.append(3)
-    elif match3:
         classes.append(2)
     else:
         classes.append(1)
+
+# Write Classes Result
+wb = Workbook()
+ws = wb.active
+for i in range(len(classes)):
+    ws.cell(row=i+1, column=1).value = classes[i]
+wb.save('result_pattern_matching.xlsx')
 
 # Read Target Classes
 target = RAWfile.readDataClass('hadits_dist_172.xlsx')
